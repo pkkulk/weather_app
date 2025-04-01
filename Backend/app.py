4ef9f4000004500feed51b91ad77af03e918d66b
Prathmesh kulkarni <prathmeshkulkarni312@gmail.com>
2025-04-01 19:14:49 +0530

    j1

--- Backend/app.py
+++ Backend/app.py
@@ -40,4 +40,5 @@ def get_weather():
         return jsonify({"error": "Server error", "details": str(e)}), 500
     
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
